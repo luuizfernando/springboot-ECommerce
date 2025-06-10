1f34acef8ed4f084a2612309b69c32268c4f1d01
Luiz Fernando Santana Dal Pra <113379005+luuizfernando@users.noreply.github.com>
2025-06-10 16:02:11 -0300

    feat/OrderItem, many-to-many association with extra attributes - PART 2

--- src/main/java/com/project/course/config/TestConfig.java
+++ src/main/java/com/project/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.project.course.entities.Category;
 import com.project.course.entities.Order;
+import com.project.course.entities.OrderItem;
 import com.project.course.entities.Product;
 import com.project.course.entities.User;
 import com.project.course.entities.enums.OrderStatus;
 import com.project.course.repositories.CategoryRepository;
+import com.project.course.repositories.OrderItemRepository;
 import com.project.course.repositories.OrderRepository;
 import com.project.course.repositories.ProductRepository;
 import com.project.course.repositories.UserRepository;
@@ -32,6 +34,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {	
@@ -67,5 +72,11 @@ public class TestConfig implements CommandLineRunner {
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT,u1);
 		// Salva os objetos o1, o2 e o3 no banco de dados
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 }
