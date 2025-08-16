83f64a12dd9685e4d8458d25aae9a8f099f5cd4d
Luiz Fernando <luizfernandosant26@gmail.com>
2025-08-16 20:19:03 -0300

    fix/ ID type of Order Item

--- src/main/java/com/project/course/repositories/OrderItemRepository.java
+++ src/main/java/com/project/course/repositories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package com.project.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.course.entities.OrderItem;
+import com.project.course.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
 	
 }
